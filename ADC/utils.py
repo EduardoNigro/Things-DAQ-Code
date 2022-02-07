99c5bbc262e2d4dfaa52209e4a36c38da1d22540
Eduardo Nigro <eduardo.nigro@navistar.com>
2022-02-06 21:11:40 -0600

    Updated plot_line

--- ADC/utils.py
+++ ADC/utils.py
@@ -2,11 +2,11 @@
 
 Contains a useful plotting function that is used in the coding examples.
 The function was built using Plotly instead of Matplotlib due to its
-interactive graphs.
+interactive graphs and because it runs better on Raspberry Pi Linux.
 
 Author: Eduardo Nigro
-    rev 0.0.5
-    2021-12-26
+    rev 0.0.6
+    2022-01-24
 """
 import numpy as np
 import plotly.io as pio
@@ -20,17 +20,22 @@ pio.templates.default = mytemplate
 
 
 def plot_line(
-    x, y, xname="Time (s)", yname=None, axes="single", marker=False, legend=None):
+    x, y, xname="Time (s)", yname=None, axes="single",
+    figsize=None, line=True, marker=False, legend=None):
     """
     Plot lines using plotly.
 
     :param x: x values for plotting.
-    :type x: list(float)
+        List or ndarray. List of lists or ndarrays is also supported. 
+    :type x: list(float), list(list), list(ndarray)
 
     :param y: y values for plotting.
-    :type y: list(float)
+        List or ndarray. List of lists or ndarrays is also supported. 
+    :type y: list(float), list(list), list(ndarray)
 
     :param xname: The x axis title. Default value is ``'Time (s)'``.
+        If ``'Angle (deg.)'`` is used, axes ticks are configured in 360 degree
+        increments.
     :type xname: str
 
     :param yname: The y axis title.
@@ -43,20 +48,47 @@ def plot_line(
         If ``'multi'``, each curve is plotted on its own axis.
     :type axes: str
 
+    :param figsize: The figure size (``width``, ``height``) in pixels.
+    :type figsize: tuple(int)
+
+    :param line: Displays the curves if ``True``.
+    :type line: bool, list(bool)
+
     :param marker: Displays markers on the curves if ``True``.
-    :type marker: bool
+    :type marker: bool, list(bool)
 
     :param legend: List of legend names for multiple curves.
         Length of `legend` must be the same as length of `y`.
     :type legend: list(str)
 
-
+    Example:
+        >>> import numpy as np
+        >>> from utils import plot_line
+        >>> t = np.linspace(0,2,100)
+        >>> y0 = np.sin(1*np.pi*t)
+        >>> y1 = np.cos(1*np.pi*t)    
+        >>> plot_line(
+                [t]*2, [y0, y1],
+                yname=['sin & cos'],
+                legend=['sin(pi x t)', 'cos(pi x t)']
+                )
+                
     """
-    # Adjusting inputs
+    # Making sure x and y inputs are put in lists if needed
     if type(x) != list:
         x = [x]
+    else:
+        if type(x[0]) not in [list, np.ndarray]:
+            x = [x]
     if type(y) != list:
         y = [y]
+    else:
+        if type(y[0]) not in [list, np.ndarray]:
+            y = [y]
+    # Doing a simple check for consistent x and y inputs
+    if len(x) != len(y):
+        raise Exception("'x' and 'y' inputs must have the same length.")
+    # Adjusting y axis title based on input
     if not yname:
         yname = ["y" + str(i) for i in range(len(y))]
     elif type(yname) != list:
@@ -95,35 +127,54 @@ def plot_line(
         colors = ["rgb(50, 100, 150)"] * len(y)
     else:
         raise Exception("Valid axes options are: 'single' or 'multi'.")
-    # Checking for marker options
-    if marker:
-        mode = "lines+markers"
-    else:
-        mode = "lines"
+    # Checking for line and marker options
+    if type(line) != list:
+        line = [line] * len(y)
+    if type(marker) != list:
+        marker = [marker] * len(y)
+    mode = []
+    markersize = []
+    for linei, markeri in zip(line, marker):
+        if linei and markeri:
+            mode.append("lines+markers")
+            markersize.append(2)
+        elif linei and not markeri:
+            mode.append("lines")
+            markersize.append(2)
+        elif not linei and markeri:
+            mode.append("markers")
+            markersize.append(8)
     # Setting figure parameters
+    if figsize:
+        wfig, hfig = figsize
+    else:
+        wfig = 650
+        hfig = 100 + 150*naxes
     m0 = 10
     margin = dict(l=6 * m0, r=3 * m0, t=3 * m0, b=3 * m0)
-    wfig = 750
-    hfig = 100 + 150 * naxes
     # Plotting results
     fig = make_subplots(rows=naxes, cols=1)
-    for i, xi, yi, ynamei, legendi, color in zip(iaxes, x, y, yname, legend, colors):
+    for i, xi, yi, ynamei, legendi, colori, modei, markersizei in zip(
+            iaxes, x, y, yname, legend, colors, mode, markersize):
+        # Adding x, y traces to appropriate plot
         fig.add_trace(
             go.Scatter(
                 x=xi,
                 y=yi,
                 name=legendi,
-                mode=mode,
-                line=dict(width=1, color=color),
-                marker=dict(size=2, color=color),
+                mode=modei,
+                line=dict(width=1, color=colori),
+                marker=dict(size=markersizei, color=colori),
             ),
             row=i + 1,
             col=1,
         )
-        fig.update_yaxes(title_text=ynamei, row=i + 1, col=1)
+        # Adding x axes ticks
         if xname.lower().find("angle") < 0:
+            # Regular x axes
             fig.update_xaxes(matches="x", row=i + 1, col=1)
         else:
+            # Special case where x axes has angular values
             fig.update_xaxes(
                 tickmode="array",
                 tickvals=np.arange(0, np.round(xi[-1]) + 360, 360),
@@ -131,7 +182,11 @@ def plot_line(
                 row=i + 1,
                 col=1,
             )
-
+        # Adding y axis title to all plots
+        fig.update_yaxes(title_text=ynamei, row=i + 1, col=1)
+    # Adding x axis title to bottom plot only
     fig.update_xaxes(title_text=xname, row=i + 1, col=1)
-    fig.update_layout(margin=margin, width=wfig, height=hfig, showlegend=showlegend)
+    # Applying figure size, margins, and legend
+    fig.update_layout(
+        margin=margin, width=wfig, height=hfig, showlegend=showlegend)
     fig.show()
--- Prescribed-PWM/utils.py
+++ Prescribed-PWM/utils.py
@@ -7,7 +7,6 @@ interactive graphs and because it runs better on Raspberry Pi Linux.
 Author: Eduardo Nigro
     rev 0.0.6
     2022-01-24
-    
 """
 import numpy as np
 import plotly.io as pio
@@ -27,12 +26,16 @@ def plot_line(
     Plot lines using plotly.
 
     :param x: x values for plotting.
-    :type x: list(float)
+        List or ndarray. List of lists or ndarrays is also supported. 
+    :type x: list(float), list(list), list(ndarray)
 
     :param y: y values for plotting.
-    :type y: list(float)
+        List or ndarray. List of lists or ndarrays is also supported. 
+    :type y: list(float), list(list), list(ndarray)
 
     :param xname: The x axis title. Default value is ``'Time (s)'``.
+        If ``'Angle (deg.)'`` is used, axes ticks are configured in 360 degree
+        increments.
     :type xname: str
 
     :param yname: The y axis title.
@@ -49,22 +52,43 @@ def plot_line(
     :type figsize: tuple(int)
 
     :param line: Displays the curves if ``True``.
-    :type line: bool
+    :type line: bool, list(bool)
 
     :param marker: Displays markers on the curves if ``True``.
-    :type marker: bool
+    :type marker: bool, list(bool)
 
     :param legend: List of legend names for multiple curves.
         Length of `legend` must be the same as length of `y`.
     :type legend: list(str)
 
-
+    Example:
+        >>> import numpy as np
+        >>> from utils import plot_line
+        >>> t = np.linspace(0,2,100)
+        >>> y0 = np.sin(1*np.pi*t)
+        >>> y1 = np.cos(1*np.pi*t)    
+        >>> plot_line(
+                [t]*2, [y0, y1],
+                yname=['sin & cos'],
+                legend=['sin(pi x t)', 'cos(pi x t)']
+                )
+                
     """
-    # Adjusting inputs
+    # Making sure x and y inputs are put in lists if needed
     if type(x) != list:
         x = [x]
+    else:
+        if type(x[0]) not in [list, np.ndarray]:
+            x = [x]
     if type(y) != list:
         y = [y]
+    else:
+        if type(y[0]) not in [list, np.ndarray]:
+            y = [y]
+    # Doing a simple check for consistent x and y inputs
+    if len(x) != len(y):
+        raise Exception("'x' and 'y' inputs must have the same length.")
+    # Adjusting y axis title based on input
     if not yname:
         yname = ["y" + str(i) for i in range(len(y))]
     elif type(yname) != list:
@@ -104,44 +128,53 @@ def plot_line(
     else:
         raise Exception("Valid axes options are: 'single' or 'multi'.")
     # Checking for line and marker options
-    if line and marker:
-        mode = "lines+markers"
-        markersize = 2
-    elif line and not marker:
-        mode = "lines"
-        markersize = 2
-    elif not line and marker:
-        mode = "markers"
-        markersize = 10
+    if type(line) != list:
+        line = [line] * len(y)
+    if type(marker) != list:
+        marker = [marker] * len(y)
+    mode = []
+    markersize = []
+    for linei, markeri in zip(line, marker):
+        if linei and markeri:
+            mode.append("lines+markers")
+            markersize.append(2)
+        elif linei and not markeri:
+            mode.append("lines")
+            markersize.append(2)
+        elif not linei and markeri:
+            mode.append("markers")
+            markersize.append(8)
     # Setting figure parameters
     if figsize:
         wfig, hfig = figsize
     else:
-        wfig = 750
-        hfig = 100 + 150 * naxes
+        wfig = 650
+        hfig = 100 + 150*naxes
     m0 = 10
     margin = dict(l=6 * m0, r=3 * m0, t=3 * m0, b=3 * m0)
     # Plotting results
     fig = make_subplots(rows=naxes, cols=1)
-    for i, xi, yi, ynamei, legendi, color in zip(iaxes, x, y, yname, legend, colors):
+    for i, xi, yi, ynamei, legendi, colori, modei, markersizei in zip(
+            iaxes, x, y, yname, legend, colors, mode, markersize):
+        # Adding x, y traces to appropriate plot
         fig.add_trace(
             go.Scatter(
                 x=xi,
                 y=yi,
                 name=legendi,
-                mode=mode,
-                line=dict(width=1, color=color),
-                marker=dict(size=markersize, color=color),
+                mode=modei,
+                line=dict(width=1, color=colori),
+                marker=dict(size=markersizei, color=colori),
             ),
             row=i + 1,
             col=1,
         )
         # Adding x axes ticks
         if xname.lower().find("angle") < 0:
-            # Special case where x axes has angular values
+            # Regular x axes
             fig.update_xaxes(matches="x", row=i + 1, col=1)
         else:
-            # Regular x axes
+            # Special case where x axes has angular values
             fig.update_xaxes(
                 tickmode="array",
                 tickvals=np.arange(0, np.round(xi[-1]) + 360, 360),
@@ -149,12 +182,11 @@ def plot_line(
                 row=i + 1,
                 col=1,
             )
-        # Adding y axes title to all plots
+        # Adding y axis title to all plots
         fig.update_yaxes(title_text=ynamei, row=i + 1, col=1)
-    # Adding x axes title to bottom plot only
+    # Adding x axis title to bottom plot only
     fig.update_xaxes(title_text=xname, row=i + 1, col=1)
-    # Applying figure size, margins and legend
+    # Applying figure size, margins, and legend
     fig.update_layout(
-        margin=margin, width=wfig, height=hfig, showlegend=showlegend
-    )
+        margin=margin, width=wfig, height=hfig, showlegend=showlegend)
     fig.show()
